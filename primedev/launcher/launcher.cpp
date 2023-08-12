f693a2e857a7bf87fe27f08ac90ce49209e8155d
F1F7Y <filip.bartos07@proton.me>
2023-08-12 22:02:13 +0200

    Log `CSourceAppSystemGroup__PreInit`

--- primedev/launcher/launcher.cpp
+++ primedev/launcher/launcher.cpp
@@ -1,6 +1,12 @@
 
 AUTOHOOK_INIT()
 
+AUTOHOOK(CSourceAppSystemGroup__PreInit, launcher.dll + 0xB730, bool, __fastcall, (void* thisptr))
+{
+	DevMsg(eLog::ENGINE, "CSourceAppSystemGroup__PreInit\n");
+	return CSourceAppSystemGroup__PreInit(thisptr);
+}
+
 // clang-format off
 AUTOHOOK_PROCADDRESS(LauncherMain, launcher.dll, LauncherMain, int, __fastcall,
 	(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow))
