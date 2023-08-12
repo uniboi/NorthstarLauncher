77e9e2df31a7ab56557bc6d7511370ee1d838f5a
F1F7Y <filip.bartos07@proton.me>
2023-08-12 21:58:51 +0200

    Deprecate `autoexec_ns_*`

--- primedev/engine/host.cpp
+++ primedev/engine/host.cpp
@@ -13,12 +13,15 @@ AUTOHOOK(Host_Init, engine.dll + 0x155EA0,
 void, __fastcall, (bool bDedicated))
 // clang-format on
 {
-	DevMsg(eLog::ENGINE, "Host_Init()\n");
+	DevMsg(eLog::ENGINE, "Host_Init(bDedicated: %d)\n", bDedicated);
 	Host_Init(bDedicated);
 	FixupCvarFlags();
 	// need to initialise these after host_init since they do stuff to preexisting concommands/convars without being client/server specific
 	InitialiseCommandPrint();
 	InitialiseMapsPrint();
+
+	// Hardcoded mod functionality, add something to mod.json or just teach users on how to +exec as a replacement ot this
+	Warning(eLog::ENGINE, "'autoexec_ns_*' files are deprecated!");
 	// client/server autoexecs on necessary platforms
 	// dedi needs autoexec_ns_server on boot, while non-dedi will run it on on listen server start
 	if (bDedicated)
