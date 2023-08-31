4bbfc3f28e92c4c809826ef854859605ca102c97
F1F7Y <filip.bartos07@proton.me>
2023-08-31 19:42:31 +0200

    Move rui_draw to `rtech/rui/rui.cpp`

--- primedev/rtech/rui/rui.cpp
+++ primedev/rtech/rui/rui.cpp
@@ -1,19 +1,18 @@
-#include "tier1/convar.h"
 
 AUTOHOOK_INIT()
 
 // clang-format off
-AUTOHOOK(DrawRUIFunc, engine.dll + 0xFC500,
+AUTOHOOK(RUI_Draw, engine.dll + 0xFC500,
 bool, __fastcall, (void* a1, float* a2))
 // clang-format on
 {
 	if (!Cvar_rui_drawEnable->GetBool())
 		return 0;
 
-	return DrawRUIFunc(a1, a2);
+	return RUI_Draw(a1, a2);
 }
 
-ON_DLL_LOAD_CLIENT_RELIESON("engine.dll", RUI, ConVar, (CModule module))
+ON_DLL_LOAD_CLIENT_RELIESON("engine.dll", RTechRUIRUI, ConVar, (CModule module))
 {
 	AUTOHOOK_DISPATCH()
 }
